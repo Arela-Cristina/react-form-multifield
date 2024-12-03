87e371a37c3c9b0e8e9b335e023481c18e7d9665
Arela-Cristina <instagrandine@gmail.com>
2024-12-03 14:31:59 +0100

    add basic structure

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,12 +1,16 @@
-import './Index.css'
+import "./Index.css";
+import Header from "./components/header-layout/Header.jsx";
+import Body from "./components/body-layout/Body";
+import Footer from "./components/footer-layout/Footer";
 
 function Index() {
-
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
+      <Body />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Index
+export default Index;
